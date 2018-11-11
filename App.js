8a4cc2090f005f8793dbb1bbf297c2bd9c4a4522
silvestrevivo <silvestrevivomillan@gmail.com>
2018-11-11 23:26:52 +0100

    first aproach to app

---
+++ App.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+
+import { connect } from 'react-redux';
+import * as actions from './src/store/actions'
+
+class App extends Component {
+
+  placeAddedHandler = placeName => {
+    this.props.addPlace(placeName);
+  };
+
+  placeDeletedHandler = () => {
+    this.props.deletePlace();
+  };
+
+  modalClosedHandler = () => {
+    this.props.deselectPlace();
+  };
+
+  placeSelectedHandler = key => {
+    this.props.selectPlace(key);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace={this.props.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.props.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  }
+}
+
+
+export default connect(mapStateToProps, actions)(App)
